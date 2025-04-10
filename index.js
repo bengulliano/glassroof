34b4469a4cf153d2600a334e82836e91e7f962ad
bengulliano <bengulliano@gmail.com>
2025-04-10 18:22:12 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoURI = require('dotenv').config();
+const App = express();
+const port = process.env.PORT || 3000;
+
+// MongoDB Atlas connection imported from .env file
+// Example: mongoURI = 'mongodb+srv://<username>:<password>@cluster0.abcde.mongodb.net/myapp?retryWrites=true&w=majority'
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas:glasswindow'))
+    .catch(err => console.error('Connection error:', err));
+
+// Simple route
+App.get('/', (req, res) => {
+    res.send('Hello from Render + MongoDB Atlas!');
+});
+
+App.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+App();
+
