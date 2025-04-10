530267144162a9612034cd9b97c869687024c9b8
bengulliano <bengulliano@gmail.com>
2025-04-10 20:46:11 +0200

    added support for dotenv

--- index.js
+++ index.js
@@ -2,14 +2,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 const App = express();
 const port = process.env.PORT || 3000;
 MONGODB_URI = process.env.MONGODB_URI
 
+
+// Middleware
+App.use(cors());
+App.use(express.json());
+
+
 // MongoDB Atlas connection imported from .env file
 // Example: mongoURI = 'mongodb+srv://<username>:<password>@cluster0.abcde.mongodb.net/myapp?retryWrites=true&w=majority'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB Atlas:glasswindow'))
+    .then(() => console.log('Connected to MongoDB Atlas:glasswindow')).then(() => {
+        App.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    })
     .catch(err => console.error('Connection error:', err));
 
 // Simple route
@@ -17,9 +28,7 @@ App.get('/', (req, res) => {
     res.send('Hello from Render + MongoDB Atlas!');
 });
 
-App.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
 
-App();
+
+//App();
 
