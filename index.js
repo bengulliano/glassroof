46ada8f329d4efc0d269a0246c2ef485b5a39258
bengulliano <bengulliano@gmail.com>
2025-04-10 19:56:59 +0200

    added support for dotenv

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const App = express();
 const port = process.env.PORT || 3000;
+MONGODB_URI = process.env.MONGODB_URI
 
 // MongoDB Atlas connection imported from .env file
 // Example: mongoURI = 'mongodb+srv://<username>:<password>@cluster0.abcde.mongodb.net/myapp?retryWrites=true&w=majority'
