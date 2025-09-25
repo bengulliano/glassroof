726859cb466546ea251a40e35900b72c1feae82a
bengulliano <bengulliano@gmail.com>
2025-09-25 08:39:46 +0200

    Build encrypted .env.vault file for deploy

--- index.js
+++ index.js
@@ -3,34 +3,46 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
+
 const App = express();
-const port = process.env.PORT || 3000;
-MONGODB_URI = process.env.MONGODB_URI
-
-
 // Middleware
 App.use(cors());
 App.use(express.json());
 
-console.log(MONGODB_URI);
+const port = process.env.PORT || 4000;
+MONGODB_URI = process.env.MONGODB_URI;
 
+async function run() {
+    const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+    try {
+        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+        await mongoose.connect(MONGODB_URI, clientOptions)
+            .then(() => console.log('Connected to MongoDB Atlas:glassroof'))
+            .then(() => {
+                App.listen(port, () => {
+                    console.log(`Server running on port ${port}`);
+                });
+            })
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+}
 
-// MongoDB Atlas connection imported from .env file
-// Example: mongoURI = 'mongodb+srv://<username>:<password>@cluster0.abcde.mongodb.net/myapp?retryWrites=true&w=majority'
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB Atlas:glasswindow')).then(() => {
-        App.listen(port, () => {
-            console.log(`Server running on port ${port}`);
-        });
-    })
-    .catch(err => console.error('Connection error:', err));
-
-// Simple route
-App.get('/', (req, res) => {
-    res.send('Hello from Render + MongoDB Atlas!');
+// Handle app termination gracefully
+process.on('SIGINT', async () => {
+    console.log('Shutting down gracefully...');
+    await mongoose.disconnect();
+    process.exit(0);
 });
 
+run().catch(console.dir);
 
 
-//App();
+
+// API Routes
+// Simple route
+App.get('/', (req, res) => {
+    res.send(`Hello from ${process.env.HELLO}`);
+});
 
